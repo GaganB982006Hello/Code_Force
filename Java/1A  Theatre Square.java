cd923ca893301c1de423a8850f7ce85637c0efbb
Gagan B <gaganb982006@gmail.com>
2026-08-02 18:27:10 +0530

    Create 1A  Theatre Square.java

---
+++ Java/1A  Theatre Square.java
@@ -0,0 +1,38 @@
+/*
+time limit per test1 second
+memory limit per test256 megabytes
+Theatre Square in the capital city of Berland has a rectangular shape with the size n × m meters. On the occasion of the city's anniversary, a decision was taken to pave the Square with square granite flagstones. Each flagstone is of the size a × a.
+
+What is the least number of flagstones needed to pave the Square? It's allowed to cover the surface larger than the Theatre Square, but the Square has to be covered. It's not allowed to break the flagstones. The sides of flagstones should be parallel to the sides of the Square.
+
+Input
+The input contains three positive integer numbers in the first line: n,  m and a (1 ≤  n, m, a ≤ 109).
+
+Output
+Write the needed number of flagstones.
+
+Examples
+InputCopy
+6 6 4
+OutputCopy
+4
+*/
+
+
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long a = sc.nextLong();
+        
+        long rows = (n + a - 1) / a;
+        long cols = (m + a - 1) / a;
+        
+        System.out.println(rows * cols);
+        sc.close();
+    }
+}
